481fa74a19599888559f64ed7b9be1ae7e3a76ce
Karan Janthe <karanjanthe@gmail.com>
2022-01-19 16:18:10 +0530

    completed the project

--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const bodyParser=require("body-parser");
+const multer=require("multer");
+const upload=multer({dest:"uploads/"});
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse",upload.single("upfile"),function(req,res){
+  // console.log(req);
+   if(req.file===undefined){
+    res.end("invalid file");
+  }
+  else{
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+    }
+})
 
 
 const port = process.env.PORT || 3000;
